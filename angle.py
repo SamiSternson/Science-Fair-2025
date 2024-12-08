4cbf52629d20b75dfbad12a7be5ac70c403659d8
SamiSternson <samisternson@gmail.com>
2024-12-08 12:37:54 -0800

    takes good pics

--- angle.py
+++ angle.py
@@ -14,29 +14,38 @@ def gcd(a, b):
     return gcd(b % a, a)
 
 
-def measure_angle():
+def measure_angle_right():
     cam = cv2.VideoCapture(cam_port)
     result, image = cam.read()
     print(result)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray_image=gray_image[320:640, 240:480]
-    img_blur = cv2.GaussianBlur(gray_image, (3, 3), 0)
+    gray_image=cv2.medianBlur(gray_image[ 240:480, 200:640], 5)
+    img_blur = cv2.GaussianBlur(gray_image, (5, 5), 0)
     dges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)
     zipped = np.dstack((gray_image, dges))
+    x = []
+    y = []
+    height, width = gray_image.shape
+    print(height, width)
+    top=None
+    for y_i, row in enumerate(dges):
+        for x_i, pixel in enumerate(row):
+            if pixel > 0.95:
+                if top==None:
+                    top=y_i
+                    left=x_i
+                    break
+    dges=dges[top:-1, left:-1]
+    for y_i, row in enumerate(dges):
+        for x_i, pixel in enumerate(row):
+            if pixel > 0.95:
+                x.append(x_i)
+                y.append(abs(y_i - len(zipped)))
     cv2.imshow("Images", dges)
     cv2.imshow("Color", image)
     cv2.imshow("gray", gray_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    x = []
-    y = []
-    height, width = gray_image.shape
-    print(height, width)
-    for y_i, row in enumerate(zipped):
-        for x_i, pixel in enumerate(row):
-            if pixel[-1] > 0.95:
-                x.append(x_i)
-                y.append(abs(y_i - len(zipped)))
     slope, intercept, r, p, std_err = stats.linregress(x, y)
     
     tick_size = gcd(height, width)
@@ -46,8 +55,50 @@ def measure_angle():
     angle_of_rep = np.rad2deg(np.arctan(abs(slope)))
     plt.yticks([i * tick_size for i in range((height // tick_size) + 1)])
     plt.show()
-
     return angle_of_rep
 
-repose=measure_angle()
+def measure_angle_left():
+    cam = cv2.VideoCapture(cam_port)
+    result, image = cam.read()
+    print(result)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray_image=cv2.medianBlur(gray_image[240:480, 0:400], 5)
+    img_blur = cv2.GaussianBlur(gray_image, (5, 5), 0)
+    dges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)
+    zipped = np.dstack((gray_image, dges))
+    x = []
+    y = []
+    height, width = gray_image.shape
+    print(height, width)
+    top=None
+    for y_i, row in enumerate(dges):
+        for x_i, pixel in enumerate(row):
+            if pixel > 0.95:
+                if top==None:
+                    top=y_i
+                    right=x_i
+                    break
+    dges=dges[top:-1, 0:right]
+    for y_i, row in enumerate(dges):
+        for x_i, pixel in enumerate(row):
+            if pixel > 0.95:
+                x.append(x_i)
+                y.append(abs(y_i - len(zipped)))
+    cv2.imshow("Images", dges)
+    cv2.imshow("Color", image)
+    cv2.imshow("gray", gray_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    slope, intercept, r, p, std_err = stats.linregress(x, y)
+    
+    tick_size = gcd(height, width)
+    line_x = np.linspace(0, width)
+    plt.plot(line_x, line_x * slope + intercept)
+    plt.scatter(x, y)
+    angle_of_rep = np.rad2deg(np.arctan(abs(slope)))
+    plt.yticks([i * tick_size for i in range((height // tick_size) + 1)])
+    plt.show()
+    return angle_of_rep
+
+repose=(measure_angle_right()+measure_angle_left())/2
 print(repose)
