e2895e7fccd3cd7d9f236b2002433a5734f2f1ed
SamiSternson <samisternson@gmail.com>
2024-12-08 15:05:22 -0800

    Added CSV stuff

--- angle.py
+++ angle.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
-import csv 
+import csv
 
 cam_port = 0
 
@@ -19,23 +19,23 @@ def measure_angle_right():
     result, image = cam.read()
     print(result)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray_image=cv2.medianBlur(gray_image[ 240:480, 200:640], 5)
-    img_blur = cv2.GaussianBlur(gray_image, (5, 5), 0)
+    gray_image = cv2.medianBlur(gray_image[50:480, 200:640], 15)
+    img_blur = cv2.GaussianBlur(gray_image, (15, 15), 0)
     dges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)
     zipped = np.dstack((gray_image, dges))
     x = []
     y = []
     height, width = gray_image.shape
     print(height, width)
-    top=None
+    top = None
     for y_i, row in enumerate(dges):
         for x_i, pixel in enumerate(row):
             if pixel > 0.95:
-                if top==None:
-                    top=y_i
-                    left=x_i
+                if top == None:
+                    top = y_i
+                    left = x_i
                     break
-    dges=dges[top:-1, left:-1]
+    dges = dges[top:-1, left:-1]
     for y_i, row in enumerate(dges):
         for x_i, pixel in enumerate(row):
             if pixel > 0.95:
@@ -47,7 +47,7 @@ def measure_angle_right():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     slope, intercept, r, p, std_err = stats.linregress(x, y)
-    
+
     tick_size = gcd(height, width)
     line_x = np.linspace(0, width)
     plt.plot(line_x, line_x * slope + intercept)
@@ -56,29 +56,30 @@ def measure_angle_right():
     plt.yticks([i * tick_size for i in range((height // tick_size) + 1)])
     plt.show()
     return angle_of_rep
+
 
 def measure_angle_left():
     cam = cv2.VideoCapture(cam_port)
     result, image = cam.read()
     print(result)
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray_image=cv2.medianBlur(gray_image[240:480, 0:400], 5)
-    img_blur = cv2.GaussianBlur(gray_image, (5, 5), 0)
+    gray_image = cv2.medianBlur(gray_image[50:480, 0:400], 15)
+    img_blur = cv2.GaussianBlur(gray_image, (15, 15), 0)
     dges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200)
     zipped = np.dstack((gray_image, dges))
     x = []
     y = []
     height, width = gray_image.shape
     print(height, width)
-    top=None
+    top = None
     for y_i, row in enumerate(dges):
         for x_i, pixel in enumerate(row):
             if pixel > 0.95:
-                if top==None:
-                    top=y_i
-                    right=x_i
+                if top == None:
+                    top = y_i
+                    right = x_i
                     break
-    dges=dges[top:-1, 0:right]
+    dges = dges[top:-1, 0:right]
     for y_i, row in enumerate(dges):
         for x_i, pixel in enumerate(row):
             if pixel > 0.95:
@@ -90,7 +91,7 @@ def measure_angle_left():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     slope, intercept, r, p, std_err = stats.linregress(x, y)
-    
+
     tick_size = gcd(height, width)
     line_x = np.linspace(0, width)
     plt.plot(line_x, line_x * slope + intercept)
@@ -100,5 +101,23 @@ def measure_angle_left():
     plt.show()
     return angle_of_rep
 
-repose=(measure_angle_right()+measure_angle_left())/2
-print(repose)
+
+print("File Name?")
+csv_file_path = input()
+with open(csv_file_path, "r") as file:
+    reader = csv.reader(file)
+    data = list(reader)
+    while True:
+        command = input().lower()
+        if command == "kill" or command == "done":
+            break
+        elif command == "new column" or command == "nc":
+            print("Column Name?")
+            cname = input()
+            data[0] = cname
+        elif command == "new entry" or command == "ne":
+            repose = (measure_angle_right() + measure_angle_left()) / 2
+            data[cname].append(repose)
+with open(csv_file_path, "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
