3a81e6dff7ee03b720fab7c4826ec24df63bd326
SamiSternson <samisternson@gmail.com>
2025-03-07 17:57:31 -0800

    Added A bunch of Graph stuff and underwater experiment stuff

--- linear_regression_as_full_file.py
+++ linear_regression_as_full_file.py
@@ -1,17 +1,11 @@
 # %%
-
-
-# %%
 # %%
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error
-from sklearn import preprocessing
-import math
+import matplotlib.pylab as pylab
+import sklearn.linear_model as linear
+import numpy as np
 
 # %%
 # %%
@@ -37,7 +31,7 @@ z_scored_ys = stats.zscore(ys)
 
 xs = []
 for i in range(1, len(list(df.iloc[1:,]))):
-    xs.append([list(df.iloc[i - 1 :, i])[j] for j in range(0, 40, 10)])
+    xs.append([list(df.iloc[i - 1 :, i])[j] for j in range(0, len(ys), 10)])
 # for i in range(len(xs)):
 #     xs[i]=[val**0.25 for val in xs[i]]
 zd_xs = [list(stats.zscore(x)) for x in xs]
@@ -60,85 +54,154 @@ zd_df = pd.DataFrame(
         "Angle of Repose": z_scored_ys,
         "Length to Width Ratio": new_skeleton[0],
         "Coeff of Friction": new_skeleton[1],
-        "Density": new_skeleton[2],
+        "Mass": new_skeleton[2],
         "Volume": new_skeleton[3],
-        "Length to Width Ratio s.d": new_skeleton[4],
-        "Coeff of Friction s.d": new_skeleton[5],
+        "Length to Width Ratio s.d.": new_skeleton[4],
+        "Coeff of Friction s.d.": new_skeleton[5],
+        "Volume s.d.": new_skeleton[6],
+        "Mass s.d.": new_skeleton[7],
     }
 )
 zd_df
 
 # %%
 # %%
-
-plt.scatter(df["Angle of Repose"], zd_df["Angle of Repose"])
+plt.rcParams.update({"font.size": 14})
+fig, axs = plt.subplots(2, 2, figsize=(7, 5))
+x_ = ["Mass", "Mass s.d.", "Volume", "Volume s.d."]
+total = 0
+for j in range(2):
+    total += j
+    for k in range(2):
+        total += k
+        print(total)
+        x = x_[total]
+        axs[j, k].scatter(df[x], df["Angle of Repose"], c="k", alpha=0.1)
+        average_y_groups = [
+            np.mean(np.array(df["Angle of Repose"][10 * i : 10 * i + 10 - 1]))
+            for i in range(len(df["Angle of Repose"]) // 10)
+        ]
+        axs[j, k].scatter(
+            [df[x][i] for i in range(0, 50, 10)], average_y_groups, c="r", s=100
+        )
+        axs[j, k].set_xlabel(["Mass", "Mass s.d.", "Volume", "Volume s.d."][total])
+        axs[j, k].set_ylabel("AOR (Degrees)")
+fig.tight_layout(pad=1.15)
 plt.show()
 
 # %%
+
+
 # %%
 
 y = zd_df["Angle of Repose"]
 X = zd_df.drop(["Angle of Repose"], axis=1).astype("float64")
 
-# %%
-# %%
 
-from sklearn.linear_model import LassoCV, Lasso
+# %%
+model = linear.LassoCV(cv=10, random_state=0, max_iter=100000)
 
-avg_error = 0
-model_scores = []
-all_coeffs = []
-for i in range(0, 10):
-    X_train = X.drop([(i + j * 10) for j in range(4)])
-    y_train = y.drop([(i + j * 10) for j in range(4)])
-    X_test = X.drop(X_train.index)
-    y_test = y.drop(y_train.index)
-    model = LassoCV(cv=len(X_train), random_state=10, max_iter=100000)
-    model.fit(X_train, y_train)
-    lasso_best = Lasso(alpha=model.alpha_)
-    lasso_best.fit(X_train, y_train)
-    error = mean_squared_error(y_test, lasso_best.predict(X_test))
-    avg_error += error
+# Fit model
+model.fit(X, y)
+plt.semilogx(model.alphas_, model.mse_path_, ":")
+plt.plot(
+    model.alphas_,
+    model.mse_path_.mean(axis=-1),
+    "k",
+    linewidth=2,
+)
+plt.axvline(
+    model.alpha_,
+    linestyle="--",
+    color="k",
+)
+
+
+plt.xlabel("Alpha")
+plt.ylabel("Mean square error")
+plt.title("Mean square error on each fold")
+plt.axis("tight")
+
+ymin, ymax = 0, 1.75
+plt.ylim(ymin, ymax)
+
+# %%
+r_squareds = []
+coeffs_all_models = []
+for i in range(8):
+    lasso_best = linear.Lasso(alpha=0.032600090499721894)
+    lasso_best.fit(X, y)
+    print(lasso_best.get_params())
     coeffs = list(
         lasso_best.coef_,
     )
-    all_coeffs.append(coeffs)
-    test_score = lasso_best.score(X_test, y_test)
-    train_score = lasso_best.score(X_train, y_train)
-    model_scores.append(test_score)
-    print(test_score)
-avg_error /= 10
-print("Median Score:", np.median(np.array(sorted(model_scores))))
-print(avg_error)
+    train_score = lasso_best.score(X, y)
+    r_squareds.append(train_score)
+    coeffs_all_models.append(
+        list(zip([column.replace(" ", "\n") for column in X.columns], coeffs))
+    )
+    print("Model", i + 1)
+    print("R Squared:", train_score)
+    print(list(zip([column.replace(" ", "\n") for column in X.columns], coeffs)))
+    coeffs = [abs(coeff) for coeff in coeffs]
+    print("Dropping", X.columns[coeffs.index(max(coeffs))])
+    X = X.drop(X.columns[coeffs.index(max(coeffs))], axis=1)
 
-plt.violinplot(model_scores, showmedians=True)
+    print("\n")
+
+
+# %%
+plt.rcParams.update({"font.size": 14})
+x = [i for i in range(8)]
+plt.plot(x, r_squareds, linewidth=3)
+plt.scatter(x, r_squareds, c="r")
+plt.xticks(np.arange(min(x), max(x) + 1, 1.0))
+plt.xlabel("Number of Features Removed from Model")
+plt.ylabel("R-Squared Value of Model")
+plt.title("Model-explained variance with feature elimination")
 plt.show()
 
 # %%
-
-grouped_coeffs = [[] for i in range(len(all_coeffs[0]))]
-for i in range(len(all_coeffs[0])):
-    for coeffs in all_coeffs:
-        grouped_coeffs[i].append(coeffs[i])
-
-print(grouped_coeffs)
+plt.rcParams.update({"font.size": 14})
+fig, ax = plt.subplots(figsize=(7, 4))
+ax.set_ylim(-0.6, 0.6)
+local_x = [coeff[0] for coeff in coeffs_all_models[0]]
+local_y = [coeff[1] for coeff in coeffs_all_models[0]]
+ax.axhline(0, linestyle="dotted", c="k")
+ax.scatter(local_x, local_y, c="r", marker="+", s=200)
+ax.set_xlabel("Attributes")
+ax.set_ylabel("Coefficient Values")
+ax.set_title("Coefficients for multiple linear regression model")
+plt.show()
 
 # %%
-import re
+wet_AORS = [
+    36.70639496,
+    44.14815339,
+    42.31637803,
+    41.7178955,
+    49.13631135,
+    40.71381502,
+    41.92704327,
+    47.57090818,
+    32.42581098,
+    44.22456394,
+]
 
-plt.rcParams.update({"font.size": 12})
-labels = list(X.columns)
-labels[-1] = "Coeff of Friction\ns.d"
-labels = [re.sub(r"(\s\S*?)\s", r"\1\n ", label) for label in labels]
-avg_coeffs = [np.mean(np.array(coeff)) for coeff in grouped_coeffs]
-coeffs_std = [np.std(np.array(coeff)) for coeff in grouped_coeffs]
-plt.bar(labels, avg_coeffs, yerr=coeffs_std, capsize=10)
-plt.scatter(
-    [label for label in labels for i in range(10)],
-    [coeff for group in grouped_coeffs for coeff in group],
+fig, ax = plt.subplots(figsize=(3, 5))
+dry_AORs = list(df["Angle of Repose"][40:])
+xs = ["Pile" for i in range(10)] + ["Submerged Pile" for i in range(10)]
+ax.scatter(xs, dry_AORs + wet_AORS, c="k", alpha=0.1)
+ax.scatter([""], 45, alpha=0)
+ax.scatter(
+    ["Pile", "Submerged Pile"],
+    [np.mean(np.array(dry_AORs)), np.mean(np.array(wet_AORS))],
+    c="r",
+    s=200,
 )
-plt.xlabel("Coefficients")
-plt.ylabel("Coefficients returned by LASSO regression")
-plt.yticks(fontsize=12)
-plt.xticks(fontsize=10)
+ax.set_ylabel("AOR")
+ax.set_xlabel("Pile Types")
+print(dry_AORs)
 plt.show()
+
+# %%
